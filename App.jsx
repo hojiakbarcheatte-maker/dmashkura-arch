c0c99a03b4364774ae189cae18299145ac42f793
mercy <dmashkura@gmail.com>
2025-11-23 11:44:46 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import CardParent from "./components/Card-Parent/Card-Parent";
+
+function App() {
+	return (
+		<>
+		<div className="container">
+			<div className="parent">
+				<CardParent />
+			</div>
+		</div>
+		</>
+	);
+}
+
+export default App;
